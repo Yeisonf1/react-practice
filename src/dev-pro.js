48a9b9ca3a2eb1c65d764ba804a22de829febf24
NameComplet <namecomplet@fake8364.fake8364>
2022-11-28 23:16:57 -0500

    Probando el react tutorial, elementos en componentes

---
+++ src/dev-pro.js
@@ -0,0 +1,16 @@
+    const nam = 'Rodolfo';
+    const int = '?';
+    
+
+
+    
+function Solo(){
+    const element = <h1>Holas {nam}, como has estado{int}</h1>;
+    return(
+        <div>{element}</div>
+    
+    )
+}
+
+
+export default Solo;
